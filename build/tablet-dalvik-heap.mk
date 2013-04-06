d759734879d01578be8960a23686757122899857
liquid0624 <rjbirnesser@gmail.com>
2013-04-05 21:37:34 -0400

    Tweak Global Dalvik Cache Heapsizes These files can be included in your device tree or common repos
    Conflicts:
    	build/phone-hdpi-512-dalvik-heap.mk
    	build/phone-xhdpi-1024-dalvik-heap.mk

--- build/tablet-dalvik-heap.mk
+++ build/tablet-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a standard tablet device.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=256m \
+    dalvik.vm.heapstartsize=32m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=2m
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
