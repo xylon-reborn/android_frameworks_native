74fa945a0c930b09155366b950d1e1006146ea04
liquid0624 <rjbirnesser@gmail.com>
2013-04-29 13:25:46 -0400

    Realign Dalvik Heapsizes With AOSP

--- build/phone-hdpi-512-dalvik-heap.mk
+++ build/phone-hdpi-512-dalvik-heap.mk
@@ -18,9 +18,9 @@
 # phone with around 512MB total RAM.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapstartsize=5m \
+    dalvik.vm.heapgrowthlimit=48m \
     dalvik.vm.heapsize=128m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=2m
--- build/phone-xhdpi-1024-dalvik-heap.mk
+++ build/phone-xhdpi-1024-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a xhdpi phone
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapsize=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=384m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
