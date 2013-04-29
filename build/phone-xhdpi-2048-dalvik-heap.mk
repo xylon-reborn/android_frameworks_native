4c32fa3cd367d09546ffad70bb8ecedfdfb7340a
liquid0624 <rjbirnesser@gmail.com>
2013-04-29 13:27:53 -0400

    Adjust Dalvik Heap Start Size (XHDPI 2048)

--- build/phone-xhdpi-2048-dalvik-heap.mk
+++ build/phone-xhdpi-2048-dalvik-heap.mk
@@ -18,7 +18,7 @@
 # 192m of RAM gives enough space for 5 8 megapixel camera bitmaps in RAM.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
